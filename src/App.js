d572fa377bfee5008bc01489a6ba5412db8451d5
arcejuanma <arcejuanma@gmail.com>
2020-04-10 00:36:20 -0300

    se agrego template material UI

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
+import MenuBar from './containers/menuBar'
+
 function App() {
 
   return (
     <div className="App">
-     <h1>Hola</h1>
+     <MenuBar/>
     </div>
   );
 }
